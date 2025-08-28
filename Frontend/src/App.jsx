4aa91effef8630f2da1646d097937ee44223dbb4
narvindev <narvincg@gmail.com>
2025-08-28 20:01:40 +0530

    convert into batter code structure and then solve path routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,21 +4,20 @@ import Signup from "./Pages/signup";
 import Navbar from "./components/navbar.jsx";
 import Footer from "./components/footer.jsx";
 function App() {
-return (
-<BrowserRouter>
-<Routes>
-{/* <Route path="/signin" element={<Signin />} /> */}
-{/* <Route path="/signup" element={<Signup />} /> */}
-<Navbar/>
-<Route path="/" element={<signin />} />
-<Route path="/footer" element={<Footer />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Navbar />} />
 
-{/* default redirect */}
-<Route path="*" element={<Navigate to="/" />} />
-</Routes>
-</BrowserRouter>
-);
+        <Route path="/footer" element={<Footer />} />
+
+        {/* default redirect */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 
 export default App;
