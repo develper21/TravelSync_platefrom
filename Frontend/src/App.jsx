8359f7769dab22b1d7b950323f863a7d59b7709c
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-08-28 15:16:50 +0530

    impliment navbar and footer with responsive style

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signin from "./Pages/signin";
 import Signup from "./Pages/signup";
-
-
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
 function App() {
 return (
 <BrowserRouter>
 <Routes>
-<Route path="/signin" element={<Signin />} />
-<Route path="/signup" element={<Signup />} />
-
+{/* <Route path="/signin" element={<Signin />} /> */}
+{/* <Route path="/signup" element={<Signup />} /> */}
+<Navbar/>
+<Route path="/" element={<signin />} />
+<Route path="/footer" element={<Footer />} />
 
 {/* default redirect */}
-<Route path="*" element={<Navigate to="/signin" />} />
+<Route path="*" element={<Navigate to="/" />} />
 </Routes>
 </BrowserRouter>
 );
