86a7e300fe439b48c27231c505884160b229d8f5
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-08-31 22:39:26 +0530

    added trip planner step 2 page that's include different type of experience like adventure, relaxation , cultural etc

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar.jsx";
 import Footer from "./components/footer.jsx";
 import Homepage from "./Homepage/Homepage.jsx";
 import TripPlanner1 from "./Trip-Planner/step1.jsx";
+import TripPlanner2 from "./Trip-Planner/step2.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -14,7 +15,8 @@ function App() {
         <Route path="/" element={<Navbar />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/footer" element={<Footer />} />
-        <Route path="/Trip-planer" element={<TripPlanner1 />} />
+        <Route path="/Trip-planer1" element={<TripPlanner1 />} />
+        <Route path="/trip-planer2" element={<TripPlanner2/>}/>
         {/* default redirect */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
