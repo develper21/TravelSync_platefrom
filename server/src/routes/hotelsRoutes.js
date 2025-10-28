4e9ee010ff756f07fd8fdf6af4b739ec986c141e
develper21 <githubnarvin@gmail.com>
2025-10-28 22:51:02 +0530

    remove comments

--- server/src/routes/hotelsRoutes.js
+++ server/src/routes/hotelsRoutes.js
@@ -12,13 +12,10 @@ import { requireAuth } from '../middlewares/requireAuth.js';
 
 const router = express.Router();
 
-// Public routes (no authentication required)
 router.get('/', getHotels);
 router.get('/search', searchHotels);
 router.get('/eco-friendly', getEcoFriendlyHotels);
 router.get('/:id', getHotel);
-
-// Protected routes (admin only - for now, just require auth)
 router.post('/', requireAuth, createHotel);
 router.put('/:id', requireAuth, updateHotel);
 router.delete('/:id', requireAuth, deleteHotel);
