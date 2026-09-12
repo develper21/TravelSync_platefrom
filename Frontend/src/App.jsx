2cea7d0da33026434579385b4499c7277c9be81a
develper21 <githubnarvin@gmail.com>
2026-09-12 22:00:00 +0530

    update App.tsx merge conflict error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,80 +1,23 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Signin from "./Pages/signin";
-import Signup from "./Pages/signup";
-import Homepage from "./Homepage/Homepage.jsx";
-import TripPlanner1 from "./Trip-Planner/step1.jsx";
-import TripPlanner2 from "./Trip-Planner/step2.jsx";
-import About from "./About_us/about.jsx";
-import ContactSection from "./Contact_us/contact.jsx";
-import Blog from "./Blog/blog.jsx";
-import Dashboard from "./Dashboard/dashboard.jsx";
-import TripPlanner3 from "./Trip-Planner/step3.jsx";
-import TripPlanner4 from "./Trip-Planner/step4.jsx";
-import TripPlanner5 from "./Trip-Planner/step5.jsx";
-import Explore from "./Explore/explore.jsx";
-import Bookingexplore from "./Explore/bookingsdetail.jsx";
-import Payment from "./Payment/payment.jsx";
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/footer.jsx";
-
-
-
-function App() {
-  return (
- 
-    <BrowserRouter>
-
-      <Navbar />
-      <Routes>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />    
-        <Route path="/homepage" element={<Homepage />} />       
-        <Route path="/Trip-planer1" element={<TripPlanner1 />} />
-        <Route path="/trip-planer2" element={<TripPlanner2/>}/>
-        <Route path="/trip-planer3" element={<TripPlanner3/>}/>
-        <Route path="/trip-planer4" element={<TripPlanner4/>}/>
-        <Route path="/trip-planer5" element={<TripPlanner5/>}/>
-        <Route path="/explore" element={<Explore/>}/>
-        <Route path="/bookingexplore" element={<Bookingexplore/>}/>
-        <Route path="/payment" element={<Payment/>}/>
-        <Route path="/" element={<Navigate to="/homepage" />} />
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<ContactSection/>}/>
-        <Route path="/blog" element={<Blog/>}/>  
-        <Route path="/profile" element={<Dashboard/>}/>  
-        
-         
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-
- <Footer />
-
-    </BrowserRouter>
-  
-
-=======
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import Home from './pages/public/Home';
-import About from './pages/public/About';
-import Contact from './pages/public/Contact';
-import Blog from './pages/public/Blog';
-import BlogDetail from './pages/public/BlogDetail';
-import SignIn from './pages/auth/SignIn';
-import SignUp from './pages/auth/SignUp';
-import Profile from './pages/dashboard/Profile';
-import Payment from './pages/dashboard/Payment';
-import TripStep1 from './pages/trips/step1';
-import TripStep2 from './pages/trips/step2';
-import TripStep3 from './pages/trips/step3';
-import TripStep4 from './pages/trips/step4';
-import TripStep5 from './pages/trips/step5';
-import TripStep6 from './pages/trips/step6';
-import Explore from './pages/bookings/Explore';
-import DestinationDetail from './pages/bookings/DestinationDetail';
-import ProtectedRoute from './components/common/ProtectedRoute';
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/public/Home";
+import About from "./pages/public/About";
+import Contact from "./pages/public/Contact";
+import Blog from "./pages/public/Blog";
+import BlogDetail from "./pages/public/BlogDetail";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import Profile from "./pages/dashboard/Profile";
+import Payment from "./pages/dashboard/Payment";
+import TripStep1 from "./pages/trips/step1";
+import TripStep2 from "./pages/trips/step2";
+import TripStep3 from "./pages/trips/step3";
+import TripStep4 from "./pages/trips/step4";
+import TripStep5 from "./pages/trips/step5";
+import TripStep6 from "./pages/trips/step6";
+import Explore from "./pages/bookings/Explore";
+import DestinationDetail from "./pages/bookings/DestinationDetail";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
 function App() {
   return (
@@ -171,8 +114,8 @@ function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
